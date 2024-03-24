868c58cd2cf3eed95d8a4db3c5be769ed407a040
Harish Abinav <harishabinav09@gmail.com>
2024-03-24 16:34:37 +0530

    Initial Commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT=5555;
+
+export const mongoDBURL = 'mongodb+srv://root:root@book-store-mern.ckv2grc.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN'
---
+++ frontend/src/components/home/BookModal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BookModal = ({book,onClose}) => {
+  return (
+    <div className=''>BookModal</div>
+  )
+}
+
+export default BookModal
